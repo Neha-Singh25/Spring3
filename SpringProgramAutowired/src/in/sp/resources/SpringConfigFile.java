d0512ed5690bdea69d114f441590821eb1b9b3f3
Neha-Singh25 <singhneha252503@gmail.com>
2024-11-21 18:11:00 +0530

    prog.

---
+++ SpringProgramAutowired/src/in/sp/resources/SpringConfigFile.java
@@ -0,0 +1,33 @@
+package in.sp.resources;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import in.sp.beans.Address;
+import in.sp.beans.Student;
+
+@Configuration
+public class SpringConfigFile 
+{
+	@Bean
+	public Address createAddrObj() {
+		Address addr = new Address();
+		
+		addr.setHouseno(1001);
+		addr.setCity("Delhi");
+		addr.setPincode(251001);
+		
+		return addr;
+	}
+	
+	@Bean
+	public Student createStdObj() {
+		Student std = new Student();
+		
+		std.setRollno(25);
+		std.setName("mini");
+//		std.setAddress(createAddrObj());
+		
+		return std;
+	}
+}
